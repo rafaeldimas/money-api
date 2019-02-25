cf82fc6fa2b7ac186997c53e6cba0506dd887642
Rafael Audax <rafael@grupoaudax.com.br>
2019-02-25 14:29:20 +0000

    change routes imports, added path app/controllers

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+routes(app)
 app.use(routes)
 
 app.listen(3000)
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,17 @@
-const router = require('express').Router()
+const fs = require('fs')
+const path = require('path')
+const pathControllers = path.join(__dirname, 'app', 'controllers')
 
-// @todo Add routes application
+let controllers = {}
+fs.readdirSync(pathControllers)
+  .filter(file => (file.indexOf('.') !== 0) && (file.slice(-3) === '.js'))
+  .forEach(file => {
+    const controller = require(path.join(pathControllers, file))
+    controllers[controller.path] = controller.router
+  })
 
-module.exports = router
+module.exports = app => {
+  Object.keys(controllers).forEach(path => {
+    app.use(path, controllers[path])
+  })
+}
