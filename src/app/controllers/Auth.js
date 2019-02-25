c7517dad28804c06413cc4b2ccb615be0121442a
Rafael Audax <rafael@grupoaudax.com.br>
2019-02-25 15:25:09 +0000

    Added Auth controller

---
+++ src/app/controllers/Auth.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+
+router.use('/login', async (req, res) => {
+  //
+})
+
+router.use('/register', async (req, res) => {
+
+})
+
+module.exports = {
+  path: '/auth',
+  router
+}
