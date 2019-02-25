4483b67c74ad7473d86e82694c8c9510ba39f706
Rafael Audax <rafael@grupoaudax.com.br>
2019-02-25 15:40:17 +0000

    Added dependency bcryptjs and jsonwebtoken, create model, finished controller auth

--- src/app/controllers/Auth.js
+++ src/app/controllers/Auth.js
@@ -1,11 +1,38 @@
 const router = require('express').Router()
+const { User } = require('../models')
 
 router.post('/login', async (req, res) => {
-  //
+  const { email, password } = req.body
+
+  const user = await User.findOne({ where: { email } })
+
+  if (!user) {
+    return res.status(401).json({ message: 'User not found' })
+  }
+
+  if (!(await user.checkPassword(password))) {
+    return res.status(401).json({ message: 'Incorrect password' })
+  }
+
+  return res.json({
+    user,
+    token: user.generateToken()
+  })
 })
 
 router.post('/register', async (req, res) => {
-  //
+  const { email } = req.body
+
+  if (await User.findOne({ where: { email } })) {
+    return res.status(400).json({ message: 'User already exists' })
+  }
+
+  const user = await User.create(req.body)
+
+  return res.json({
+    user,
+    token: user.generateToken()
+  })
 })
 
 module.exports = {
